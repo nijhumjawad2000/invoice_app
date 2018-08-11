baf272849d64bc2161ef77be872f297ace9e3745
nijhum_jawad2000 <njawad@techlaunch.io>
2018-08-11 19:51:10 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,71 @@
+console.log("working")
+// Exercise 1
+var oper1 =16+8
+var oper2 =28-4
+var oper3 =8*3
+var oper4 =48/2
+
+console.log(oper3,oper1,oper2,oper4)
+
+// Exercise 2
+
+var op1=6%3
+var op2=10%2
+var op3=5%2
+
+//console.log(op1>=op2)
+
+// Exercise 3
+
+var string1="Test Grade as Follows\n"
+var string2="\t Student1: 96 \n"
+var string3="\t Comment:\"none\""
+
+//console.log(string1+string2+string3)
+
+// exercise 4
+
+var numbers = [0, 10, 20, 30 ,40, 50, 60,70, 80,90]
+//console.log(numbers[2]+numbers[4])
+
+//Exercise 5
+
+var me ={
+    firstName:"nijhum",
+    lastName:"jawad",
+    age: "26",
+    sex: "male",
+    location: "miami",
+}
+/*console.log(`my name is ${me.firstName} ${me.lastName}
+i am ${me.age} years old
+i am ${me.sex}
+i live in ${me['location']}`)*/
+
+
+
+// Exercise 6
+
+var n = 0;
+while (n < 10) {
+
+  n++;
+ // console.log(n)
+ 
+}
+
+
+
+/*for(var i=1; i<=10; i++){
+    if(i%2!==0){
+        console.log("i")
+    }
+
+}*/
+
+
+// while(){}
+
+// for(){}
+
+
